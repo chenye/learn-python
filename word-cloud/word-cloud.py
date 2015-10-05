c60fbcf47cbdd73d295989caee1aa75e91771c70
chenye <chenye84@gmail.com>
2015-10-05 01:30:55 -0700

    add function "word-count"

--- word-cloud/word-cloud.py
+++ word-cloud/word-cloud.py
@@ -1,2 +1,10 @@
-print "Test"
+
+def count_words(paragraph):
+    d = dict()
+    for word in paragraph.split():
+        d[word] = d.get(word, 0) + 1
+    return d
+
+paragraph = "abc is the name of a song that starts with abc"
+print count_words(paragraph)
 
